3f2c099eee4957a5d61df7aade41d89e11b11805
Konstantin S. <havendv@gmail.com>
2026-04-02 19:56:08 +0400

    feat: Updated OpenAPI spec (#351)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Cohere/Generated/Cohere.Models.FinetuningUpdateFinetunedModelRequest.Json.g.cs
+++ src/libs/Cohere/Generated/Cohere.Models.FinetuningUpdateFinetunedModelRequest.Json.g.cs
@@ -2,7 +2,7 @@
 
 namespace Cohere
 {
-    public sealed partial class UpdateFinetunedModelRequest
+    public sealed partial class FinetuningUpdateFinetunedModelRequest
     {
         /// <summary>
         /// Serializes the current instance to a JSON string using the provided JsonSerializerContext.
@@ -34,14 +34,14 @@ namespace Cohere
         /// <summary>
         /// Deserializes a JSON string using the provided JsonSerializerContext.
         /// </summary>
-        public static global::Cohere.UpdateFinetunedModelRequest? FromJson(
+        public static global::Cohere.FinetuningUpdateFinetunedModelRequest? FromJson(
             string json,
             global::System.Text.Json.Serialization.JsonSerializerContext jsonSerializerContext)
         {
             return global::System.Text.Json.JsonSerializer.Deserialize(
                 json,
-                typeof(global::Cohere.UpdateFinetunedModelRequest),
-                jsonSerializerContext) as global::Cohere.UpdateFinetunedModelRequest;
+                typeof(global::Cohere.FinetuningUpdateFinetunedModelRequest),
+                jsonSerializerContext) as global::Cohere.FinetuningUpdateFinetunedModelRequest;
         }
 
         /// <summary>
@@ -51,11 +51,11 @@ namespace Cohere
         [global::System.Diagnostics.CodeAnalysis.RequiresUnreferencedCode("JSON serialization and deserialization might require types that cannot be statically analyzed. Use the overload that takes a JsonTypeInfo or JsonSerializerContext, or make sure all of the required types are preserved.")]
         [global::System.Diagnostics.CodeAnalysis.RequiresDynamicCode("JSON serialization and deserialization might require types that cannot be statically analyzed and might need runtime code generation. Use System.Text.Json source generation for native AOT applications.")]
 #endif
-        public static global::Cohere.UpdateFinetunedModelRequest? FromJson(
+        public static global::Cohere.FinetuningUpdateFinetunedModelRequest? FromJson(
             string json,
             global::System.Text.Json.JsonSerializerOptions? jsonSerializerOptions = null)
         {
-            return global::System.Text.Json.JsonSerializer.Deserialize<global::Cohere.UpdateFinetunedModelRequest>(
+            return global::System.Text.Json.JsonSerializer.Deserialize<global::Cohere.FinetuningUpdateFinetunedModelRequest>(
                 json,
                 jsonSerializerOptions);
         }
@@ -63,14 +63,14 @@ namespace Cohere
         /// <summary>
         /// Deserializes a JSON stream using the provided JsonSerializerContext.
         /// </summary>
-        public static async global::System.Threading.Tasks.ValueTask<global::Cohere.UpdateFinetunedModelRequest?> FromJsonStreamAsync(
+        public static async global::System.Threading.Tasks.ValueTask<global::Cohere.FinetuningUpdateFinetunedModelRequest?> FromJsonStreamAsync(
             global::System.IO.Stream jsonStream,
             global::System.Text.Json.Serialization.JsonSerializerContext jsonSerializerContext)
         {
             return (await global::System.Text.Json.JsonSerializer.DeserializeAsync(
                 jsonStream,
-                typeof(global::Cohere.UpdateFinetunedModelRequest),
-                jsonSerializerContext).ConfigureAwait(false)) as global::Cohere.UpdateFinetunedModelRequest;
+                typeof(global::Cohere.FinetuningUpdateFinetunedModelRequest),
+                jsonSerializerContext).ConfigureAwait(false)) as global::Cohere.FinetuningUpdateFinetunedModelRequest;
         }
 
         /// <summary>
@@ -80,11 +80,11 @@ namespace Cohere
         [global::System.Diagnostics.CodeAnalysis.RequiresUnreferencedCode("JSON serialization and deserialization might require types that cannot be statically analyzed. Use the overload that takes a JsonTypeInfo or JsonSerializerContext, or make sure all of the required types are preserved.")]
         [global::System.Diagnostics.CodeAnalysis.RequiresDynamicCode("JSON serialization and deserialization might require types that cannot be statically analyzed and might need runtime code generation. Use System.Text.Json source generation for native AOT applications.")]
 #endif
-        public static global::System.Threading.Tasks.ValueTask<global::Cohere.UpdateFinetunedModelRequest?> FromJsonStreamAsync(
+        public static global::System.Threading.Tasks.ValueTask<global::Cohere.FinetuningUpdateFinetunedModelRequest?> FromJsonStreamAsync(
             global::System.IO.Stream jsonStream,
             global::System.Text.Json.JsonSerializerOptions? jsonSerializerOptions = null)
         {
-            return global::System.Text.Json.JsonSerializer.DeserializeAsync<global::Cohere.UpdateFinetunedModelRequest?>(
+            return global::System.Text.Json.JsonSerializer.DeserializeAsync<global::Cohere.FinetuningUpdateFinetunedModelRequest?>(
                 jsonStream,
                 jsonSerializerOptions);
         }
