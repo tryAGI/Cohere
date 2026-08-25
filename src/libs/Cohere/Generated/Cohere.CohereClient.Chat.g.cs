2db3acc838c4592df78426ec58c042cc36da8c1f
Konstantin S. <havendv@gmail.com>
2026-08-25 04:55:27 +0400

    feat: Updated OpenAPI spec (#401)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Cohere/Generated/Cohere.CohereClient.Chat.g.cs
+++ src/libs/Cohere/Generated/Cohere.CohereClient.Chat.g.cs
@@ -172,6 +172,10 @@ namespace Cohere
                 __httpRequest.VersionPolicy = global::System.Net.Http.HttpVersionPolicy.RequestVersionOrHigher;
 #endif
 
+                __httpRequest.Headers.TryAddWithoutValidation(
+                    "Accept",
+                    "application/json");
+
             foreach (var __authorization in __authorizations)
             {
                 if (__authorization.Type == "Http" ||
